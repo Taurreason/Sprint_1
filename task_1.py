90625464773099993d878e4cf217de1ccc835a49
Olga Kudakaeva <taur.reason@mail.ru>
2025-04-04 23:00:20 +0400

    Add task 1

---
+++ task_1.py
@@ -0,0 +1,20 @@
+time_line = '1h 45m,360s,25m,30m 120s,2h 60s'
+
+time_line=time_line.replace(',', ' ')
+time_line=time_line.split()
+
+print(time_line)
+sum = 0
+
+for i in time_line:
+    if 'h' in i:
+        number = int(i[:-1])
+        sum += number*60
+    elif 'm' in i:
+        number = int(i[:-1])
+        sum += number
+    elif 's' in i:
+        number = int(i[:-1])
+        sum += number//60
+
+print(sum)
